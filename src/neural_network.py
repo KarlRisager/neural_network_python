4a6f1439a25559504825c191292751451633c7bd
Karl Skarsholm Risager <karl.risager@gmail.com>
2023-02-17 15:40:50 +0100

    Updates

--- src/neural_network.py
+++ src/neural_network.py
@@ -6,12 +6,17 @@ from .layer_file import Layer
 class NeuralNetwork:
 
     def __init__(self):
-        self.layers = np.array([])
+        self.layers = []
+        self.Weights = []
+        self.Biases = []
     
 
 
-    def add_layer(self, width):
-        self.layers = np.append(self.layers, Layer(width))
+    def add_layer(self, width, Input = False):
+        if not(Input):
+            self.Biases.append(np.zeros(width))
+            self.Weights.append(np.zeros((len(self.layers[-1]), width)))
+        self.layers.append(np.zeros(width))
 
     
     def Forward(self):
