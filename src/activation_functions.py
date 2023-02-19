0644febb8986d56e27e7ad0ed2d1238b364f9d62
Karl Skarsholm Risager <karl.risager@gmail.com>
2023-02-19 17:38:03 +0100

    Forward functional

--- src/activation_functions.py
+++ src/activation_functions.py
@@ -1,17 +1,29 @@
 import numpy as np
 
 
-def BinStep(val):
+def BinStep_nv(val):
     if val<0:
         return 0
     else:
         return 1
     
-def Sigmoid(val):
+def Sigmoid_nv(val):
     return 1/(1+np.exp(-val))
 
-def Tanh(val):
+def Tanh_nv(val):
     return (np.exp(val)- np.exp(-val))/(np.exp(val)+ np.exp(-val))
 
-def ReLU(val, leak = 0):
+def ReLU_nv(val, leak = 0):
     return max(leak*val, val)
+
+def SoftMax(vals):
+    return_array = np.zeros(len(vals))
+    s = np.sum(np.exp(vals))
+    for i, val in enumerate(vals):
+        return_array[i] = np.exp(val)/s
+    return return_array
+
+BinStep = np.vectorize(BinStep_nv)
+Sigmoid = np.vectorize(Sigmoid_nv)
+Tanh = np.vectorize(Tanh_nv)
+ReLU = np.vectorize(ReLU_nv)
--- src/neural_network.py
+++ src/neural_network.py
@@ -1,5 +1,6 @@
 import numpy as np
-from .layer_file import Layer
+from src.activation_functions import *
+
 
 
 
@@ -14,15 +15,15 @@ class NeuralNetwork:
 
     def add_layer(self, width, Input = False):
         if not(Input):
-            self.Biases.append(np.zeros(width))
+            self.Biases.append(np.random.rand(width))
             self.Weights.append(np.random.rand(len(self.layers[-1]), width))
         self.layers.append(np.zeros(width))
 
     
-    def Forward(self, data):
+    def Forward(self, data, activation=SoftMax):
         self.layers[0] = data
         for i, layer in enumerate(self.layers[1:]):
-            self.layers[i+1] = np.dot(self.layers[i], self.Weights[i])+ self.Biases[i]
+            self.layers[i+1] = activation(np.dot(self.layers[i], self.Weights[i])+ self.Biases[i])
             print(self.layers[i+1])
 
 
