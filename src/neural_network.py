5451943dd1f6ffb8c74d464ff6280002b4aa4e1b
Karl Skarsholm Risager <karl.risager@gmail.com>
2023-02-19 17:12:59 +0100

    Forward prototype added

--- src/neural_network.py
+++ src/neural_network.py
@@ -15,12 +15,16 @@ class NeuralNetwork:
     def add_layer(self, width, Input = False):
         if not(Input):
             self.Biases.append(np.zeros(width))
-            self.Weights.append(np.zeros((len(self.layers[-1]), width)))
+            self.Weights.append(np.random.rand(len(self.layers[-1]), width))
         self.layers.append(np.zeros(width))
 
     
-    def Forward(self):
-        pass
+    def Forward(self, data):
+        self.layers[0] = data
+        for i, layer in enumerate(self.layers[1:]):
+            self.layers[i+1] = np.dot(self.layers[i], self.Weights[i])+ self.Biases[i]
+            print(self.layers[i+1])
+
 
     def BackPropegation(self):
         self
